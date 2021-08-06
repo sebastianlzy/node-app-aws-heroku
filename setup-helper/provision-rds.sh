2d35930ea1e24a38f0942396b0bbeec5727dfdb2
leesebas <leesebas@amazon.com>
2021-08-06 12:20:54 +0800

    add randomization for creation of rds instance

--- setup-helper/provision-rds.sh
+++ setup-helper/provision-rds.sh
@@ -5,8 +5,15 @@ echo -n Enter a master password:
 read -s password
 echo
 
+timestamp() {
+  date +%s # current time
+}
+
+export RDS_POSTGRES_IDENTIFIER=aws-postgres-instance-$(timestamp)
+echo $RDS_POSTGRES_IDENTIFIER
+
 aws rds create-db-instance \
-    --db-instance-identifier aws-postgres-instance \
+    --db-instance-identifier $RDS_POSTGRES_IDENTIFIER \
     --db-instance-class db.t3.micro \
     --engine postgres \
     --engine-version "13.2"\
@@ -14,9 +21,9 @@ aws rds create-db-instance \
     --master-user-password $password \
     --allocated-storage 20
 
-aws rds describe-db-instances --db-instance-identifier aws-postgres-instance > aws-postgres-instance.json
+aws rds describe-db-instances --db-instance-identifier $RDS_POSTGRES_IDENTIFIER > $RDS_POSTGRES_IDENTIFIER.json
 
-export RDS_POSTGRES_URL=$(cat ./aws-postgres-instance.json | jq -c ".[][0].Endpoint.Address")
+export RDS_POSTGRES_URL=$(cat ./$RDS_POSTGRES_IDENTIFIER.json | jq -c ".[][0].Endpoint.Address")
 export RDS_POSTGRES_PASSWORD=$password
 
 echo $RDS_POSTGRES_URL
